a9377dc5e6c9016cde07911e24acfb82f6898547
yair <yair99@gmail.com>
2023-09-08 11:05:15 +0300

    Update app.py

--- oscl/app.py
+++ oscl/app.py
@@ -25,6 +25,7 @@ def combine_oscillators(oscillators, duration, sample_rate):
 
 st.title('Multi-Oscillator App with Phase and Playback')
 st.write('a quick attempt by an LLM to make an https://noisio.de/boards/levitation-oscillator')
+st.write('source https://github.com/5shekel/llm_toys')
 st.image('https://i.imgur.com/UBwVcOo.jpeg', width=300)
 
 # Default parameters
